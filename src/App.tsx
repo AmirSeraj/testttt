73bbf25ad5a9b528a44b2f7442bff5e5054756f1
AmirSeraj <amir.seraj6990@gmail.com>
2024-07-22 14:29:53 +0330

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div style={{
+        color:'#000',
+        fontSize:'2rem',
+      }}>
+        <a href='tel: +989393935921'>با ما تماس بگیرید </a>
+      </div>
+    </>
+  )
+}
+
+export default App
